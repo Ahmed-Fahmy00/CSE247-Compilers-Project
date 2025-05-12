1a01d639a12c410051767ce17703b072c141073b
Ahmed Mohamed Fahmy Eldawy Hamed <185392994+Ahmed-Fahmy00@users.noreply.github.com>
2025-05-12 16:13:38 +0300

    added all phase 2 files

--- JASON_Compiler/JASON_Compiler.cs
+++ JASON_Compiler/JASON_Compiler.cs
@@ -9,17 +9,17 @@ namespace JASON_Compiler
     public static class JASON_Compiler
     {
         public static Scanner Jason_Scanner = new Scanner();
-        public static List<string> Lexemes = new List<string>();
+        public static Parser Jason_Parser = new Parser();
         public static List<Token> TokenStream = new List<Token>();
+        public static Node treeroot;
 
         public static void Start_Compiling(string SourceCode) //character by character
         {
             //Scanner
- 
             Jason_Scanner.StartScanning(SourceCode);
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               //ReplaceLexemesWithBrainrot();
             //Parser
-            //Sematic Analysis
-        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     private static void ReplaceLexemesWithBrainrot(){Dictionary<string, string> brainrotKeywords = new Dictionary<string, string>{{ "int", "chad" },{ "float", "gigachad" },{ "string", "rant" },{ "read", "skibidi" },{ "write", "cooked" },{ "repeat", "yap" },{ "until", "cap" },{ "if", "edgy" },{ "elseif", "flex" },{ "else", "amogus" },{ "then", "schizo" },{ "return", "bussin" },{ "main", "goon" },{ "end", "deadass" },{ "endl", "ohio" },};foreach (var token in TokenStream){if (brainrotKeywords.ContainsKey(token.lex)){token.lex = brainrotKeywords[token.lex];}}}
+            Jason_Parser.StartParsing(TokenStream);
+            treeroot = Jason_Parser.root;
+        } 
     }
 }
